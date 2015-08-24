ea246e50b3c32809c0753528db55f755ddf67b23
pascal <papou37@gmail.com>
2015-08-24 08:29:07 -0300

    premier commit

---
+++ master_pascal.ino
@@ -0,0 +1,26 @@
+
+
+
+#define presence_lampe true
+#set_temp_button false
+
+//set  and define pin
+DS1302RTC RTC(45,47,49);
+LiquidCrystal lcd(10,9,5,4,3,2);
+
+#define switch_up 34
+#define switch_down 35
+#define switch_common 36
+#define sd_card 4
+
+
+/analog pin
+#define sensor_mq135 0
+#define dht22_pin 1
+#define pot_adjust_temp-
+
+
+
+
+
+
